f567aa9b45916fa0cf10c29a7f5b6cf121b63002
Raphael2b3 <raphael.schuetz0311@gmail.com>
2021-10-11 12:46:26 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+from classes import *
+import time
+
+
+def controls_events():
+    keys_pressed = p.key.get_pressed()
+    Bomberman.currentscene.key_press_handler(keys_pressed)
+
+    events = p.event.get()
+    for event in events:
+        if event.type == p.QUIT:
+            exit()
+        else:
+            Bomberman.currentscene.event_handler(event)
+
+    Bomberman.currentscene.collision_detection()
+
+
+def render():
+    Bomberman.currentscene.draw()
+    p.display.update()
+
+
+Bomberman.currentscene = MainMenuScene()
+
+while True:
+    Bomberman.clock.tick(Bomberman.fps)
+    controls_events()
+    render()
---
+++ playground.py
@@ -0,0 +1,10 @@
+class lenkrad:
+    marke = "VW"
+    radius = 22
+
+class auto(lenkrad):
+    name = ""
+
+    def __init__(self):
+        self.radius
+
