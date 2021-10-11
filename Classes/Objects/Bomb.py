66375ae2afda67ceeca8c543be60a173d7613528
Raphael2b3 <raphael.schuetz0311@gmail.com>
2021-10-11 19:05:36 +0200

    - set the bomb cooldown to 2 seconds - made the mechanism to let the bomb explode more efficient

--- Classes/Objects/Bomb.py
+++ Classes/Objects/Bomb.py
@@ -3,8 +3,7 @@ from Classes.Objects.Bombanimation import Bombanimation
 
 
 class Bomb:
-    # TODO MAKE FPS ADAPTABLE
-    cooldown = 5 * Bomberman.fps / 10
+    cooldown = 2 * Bomberman.fps  # 2 sec
     design = p.transform.scale(p.image.load("Assets/Items/bomb100.png"), (50, 50))
 
     def __init__(self, position, player):
@@ -21,7 +20,6 @@ class Bomb:
         if self.cooldown > 0:
             Bomberman.screen.blit(self.design, self.rep)
             self.cooldown -= 1
-        elif self.animation.state < self.animation.timeInSec * Bomberman.fps:
-            self.animation.draw()
-        else:
+        elif self.animation.draw():
             self.player.bombs.pop(0)
+
